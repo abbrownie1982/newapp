a1419288196b908553337adfe5c64955c75416bb
Anne Brown <abbrownie@yahoo.com>
2017-02-16 13:13:29 -0500

    4.4 added order, view, show and route

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,13 @@
 class StaticPagesController < ApplicationController
-
-  def landing_page
-    @featured_product = Product.first
+  def about
   end
 
+  def contact
+  end
+
+  def landing_page
+  	@featured_product = Product.first
+  end
 end
+
+
